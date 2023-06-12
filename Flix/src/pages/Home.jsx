bda72e9801449ac92f438c9b06deb0e5a227533f
pedrorfs <pedrorfs11@gmail.com>
2023-06-12 15:52:16 -0300

    Edit Title

--- Flix/src/pages/Home.jsx
+++ Flix/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
 
   return ( 
     <div className='container'>
-      <h2 className='title'>Melhores Filmes</h2>
+      <h2 className='title'>Filmes</h2>
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
       {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie = {movie}/>)}
